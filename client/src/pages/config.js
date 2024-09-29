bcb950eec0a5cb682fcf5a108cf7915e8a5057b7
Yogesh Chennaboina <153359526+YogeshChenaboina@users.noreply.github.com>
2024-09-29 22:48:38 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "https://kalpvan.icu";
+const API_BASE_URL = "adeb9e526e98044f5bd67a84d46773e8-723164034.us-east-1.elb.amazonaws.com";
 export default API_BASE_URL;
